17c446b4658c65b1577de3c3f21152ff2f603464
admin <admin@iPhone.lan>
2019-07-22 13:13:57 +0800

    Update Data using Apollo Mutation componnet

---
+++ src/AddRecipe.js
@@ -0,0 +1,90 @@
+import React, { Component } from "react";
+import gql from "graphql-tag";
+import { Mutation } from "react-apollo";
+import RecipesQuery from "./RecipesQuery";
+
+const addRecipeMutation = gql`
+  mutation addRecipe($recipe: RecipeInput!) {
+    addRecipe(recipe: $recipe) {
+      id
+      title
+    }
+  }
+`;
+
+export default class AddRecipe extends Component {
+  state = {
+    title: "",
+    vegetarian: false
+  };
+
+  updateVegetarian = ({ target: { checked } }) => {
+    this.setState({ vegetarian: checked });
+  };
+
+  updateTitle = ({ target: { value } }) => {
+    this.setState({ title: value });
+  };
+
+  resetFields = () => {
+    this.setState({ title: "", vegetarian: false });
+  };
+
+  render() {
+    return (
+      <Mutation
+        mutation={addRecipeMutation}
+        refetchQueries={[
+          {
+            query: RecipesQuery,
+            variables: { vegetarian: false }
+          },
+          {
+            query: RecipesQuery,
+            variables: { vegetarian: true }
+          }
+        ]}
+        awaitRefetchQueries={true}
+      >
+        {(addRecipe, { loadding, error }) => (
+          <form
+            onSubmit={evt => {
+              evt.preventDefault();
+              addRecipe({
+                variables: {
+                  recipe: {
+                    title: this.state.title,
+                    vegetarian: this.state.vegetarian
+                  }
+                }
+              });
+              this.resetFields();
+            }}
+          >
+            <label>
+              <span>Title</span>
+              <input
+                type="text"
+                value={this.state.title}
+                onChange={this.updateTitle}
+              />
+            </label>
+            <label>
+              <input
+                type="checkbox"
+                checked={this.state.vegetarian}
+                onChange={this.updateVegetarian}
+              />
+              <span>vegetarian</span>
+            </label>
+            <div>
+              <button>Add Recipe</button>
+            </div>
+            {loadding && <p>Adding a recipe - please wait...</p>}
+            {error && <p>Error :( Please try again </p>}
+          </form>
+        )}
+      </Mutation>
+    );
+  }
+}
--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
 import Recipes from "./Recipes";
+import AddRecipe from "./AddRecipe";
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/graphql"
@@ -11,7 +12,7 @@ class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
-        <Recipes />
+        <AddRecipe />
       </ApolloProvider>
     );
   }
