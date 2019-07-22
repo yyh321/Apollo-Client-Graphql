2cb850a7bdba77c1692e85e93e6f9190b65159d9
admin <admin@iPhone.lan>
2019-07-22 12:31:28 +0800

    Provider Dynamic Arguments in a Apollo Query Component GraphQL Varibles

--- src/Recipes.js
+++ src/Recipes.js
@@ -3,28 +3,49 @@ import gql from "graphql-tag";
 import { Query } from "react-apollo";
 
 const recipesQuery = gql`
-  {
-    recipes {
+  query recipes($vegetarian: Boolean!) {
+    recipes(vegetarian: $vegetarian) {
       id
       title
     }
   }
 `;
 
-export default function() {
-  return (
-    <Query query={recipesQuery}>
-      {({ data, loading, error }) => {
-        if (loading) return <p>Loading...</p>;
-        if (error) return <p>Something went wrong</p>;
-        return (
-          <ul>
-            {data.recipes.map(({ id, title }) => (
-              <li key={id}> {title} </li>
-            ))}
-          </ul>
-        );
-      }}
-    </Query>
-  );
+export default class Recipes extends React.Component {
+  state = {
+    vegetarian: true
+  };
+  updateVegetarian = ({ target: { checked } }) => {
+    this.setState({ vegetarian: checked });
+  };
+  render() {
+    return (
+      <>
+        <label>
+          <input
+            type="checkbox"
+            checked={this.state.vegetarian}
+            onChange={this.updateVegetarian}
+          />
+          <span>vegetarian</span>
+        </label>
+        <Query
+          query={recipesQuery}
+          variables={{ vegetarian: this.state.vegetarian }}
+        >
+          {({ data, loading, error }) => {
+            if (loading) return <p>Loading...</p>;
+            if (error) return <p>Something went wrong</p>;
+            return (
+              <ul>
+                {data.recipes.map(({ id, title }) => (
+                  <li key={id}> {title} </li>
+                ))}
+              </ul>
+            );
+          }}
+        </Query>
+      </>
+    );
+  }
 }
