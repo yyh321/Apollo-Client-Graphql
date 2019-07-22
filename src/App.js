1a9f958d418d1fd1e28026e58a3f1b882ddf1f4e
admin <admin@iPhone.lan>
2019-07-22 11:55:00 +0800

    add apollo lib

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ApolloClient from "apollo-boost";
+import gql from "graphql-tag";
+import { ApolloProvider, ApolloConsumer } from "react-apollo";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql"
+});
+
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <div>
+          <h1>hello world!!</h1>
+          <ApolloConsumer>
+            {client => {
+              client
+                .query({
+                  query: gql`
+                    {
+                      recipes {
+                        id
+                        title
+                      }
+                    }
+                  `
+                })
+                .then(result => console.log(result));
+              return null;
+            }}
+          </ApolloConsumer>
+        </div>
+      </ApolloProvider>
+    );
+  }
 }
 
 export default App;
